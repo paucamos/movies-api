a3dcdd969452dc45c6d898ab4308fa974ac4ae31
paucamos <paucamos98@gmail.com>
2021-01-23 12:16:32 +0100

    first commit

---
+++ Controllers/BaseController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Movies_Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BaseController: ControllerBase
+    {
+    }
+}
